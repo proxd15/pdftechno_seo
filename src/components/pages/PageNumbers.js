a0fda79791a05f205f518f9d8e7bddaf7a089c76
PDF  Techno <pdftechno@gmail.com>
2025-07-02 15:41:20 +0530

    SEO Specialised project folder

--- src/components/pages/PageNumbers.js
+++ src/components/pages/PageNumbers.js
@@ -4,7 +4,7 @@ import Script from 'next/script';
 import dynamic from 'next/dynamic';
 import DragOverlay from '@/components/tools_utility/DragOverlay';
 
-const PDFPageNumbering = dynamic(() => import('@/components/tools/PDFPageNumbering'), { ssr: false });
+// const PDFPageNumbering = dynamic(() => import('@/components/tools/PDFPageNumbering'), { ssr: false });
 
 export default function PageNumbers() {
   return (
@@ -21,7 +21,7 @@ export default function PageNumbers() {
       />
       
       <main className="container mx-auto px-4 py-8 bg-white min-h-screen">
-        <PDFPageNumbering />
+        {/* <PDFPageNumbering /> */}
       </main>
       
       {/* Drag and drop overlay */}
